dbd97b853155ae44aefccc8160186cbb67fa3f0b
DJayanga <iit117021@std.uwu.ac.lk>
2021-11-24 00:34:05 +0530

    new

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,17 +11,36 @@ public class Main {
         final byte PERCENT = 100;
 
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Principl: ");
-        int principal = scanner.nextInt();
+        int principal = 0;
+        float monthlyInterest = 0;
+        int numberOfPayments = 0;
 
-        System.out.print("Annual Interest Rate: ");
-        float annalInterest = scanner.nextFloat();
-        float monthlyInterest = annalInterest / PERCENT / MONTHS_IN_YEAR;
+        while (true) {
+            System.out.print("Principl: ");
+            principal =scanner.nextInt();
+            if(principal >= 1000 && principal <=1_000_000)
+            break;
+            System.out.println("Please enter a valuer between 1000 and 1000000");
+        }
 
-        System.out.print("Period (Years): ");
-        byte years = scanner.nextByte();
-        int numberOfPayments = years * MONTHS_IN_YEAR;
+        while (true) {
+            System.out.print("Annual Interest Rate: ");
+            float annalInterest = scanner.nextFloat();
+            if (annalInterest >= 1 && annalInterest <=30) {
+                monthlyInterest = annalInterest / PERCENT / MONTHS_IN_YEAR;
+                break;
+            }
+            System.out.println("Enter a value between 1 and 30");
+        }
 
+        while (true) {
+            System.out.print("Period (Years): ");
+            byte years = scanner.nextByte();
+            if (years >= 1 && years <= 30) {
+                numberOfPayments = years * MONTHS_IN_YEAR;
+                break;
+            }
+        }
         double morgage = principal
                 * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
                 / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
